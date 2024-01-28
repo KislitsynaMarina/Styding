1b4d35a8306fc646f10ffb7b64d56e11fe09ce5c
MarinaKislitsyna <kislitsynamari0412@yandex.ru>
2024-01-28 19:10:34 +0300

    uniq fix - task26

--- final_project_26/myproject/health_monitoring/management/commands/load_data.py
+++ final_project_26/myproject/health_monitoring/management/commands/load_data.py
@@ -16,22 +16,26 @@ class Command(BaseCommand):
             count = 0
             for row in reader:
                 count += 1
-                temp_data.append(PatientInfo(
-                    id=row[0],
-                    age=row[1],
-                    gender=row[2],
-                    height=row[3],
-                    weight=row[4],
-                    ap_hi=row[5],
-                    ap_lo=row[6],
-                    cholesterol=row[7],
-                    gluc=row[8],
-                    smoke=row[9],
-                    alco=row[10],
-                    active=row[11],
-                    cardio=row[12],
-                ))
-                print(f'object_created {count}')
+                if PatientInfo.objects.filter(id=row[0]).exists():
+                    print(f'patient with id={row[0]} already exists!')
+                else:
+                    temp_data.append(PatientInfo(
+                        id=row[0],
+                        age=row[1],
+                        gender=row[2],
+                        height=row[3],
+                        weight=row[4],
+                        ap_hi=row[5],
+                        ap_lo=row[6],
+                        cholesterol=row[7],
+                        gluc=row[8],
+                        smoke=row[9],
+                        alco=row[10],
+                        active=row[11],
+                        cardio=row[12],
+                    ))
+                    print(f'object_created {count}')
+
         print('start bulk_create')
         PatientInfo.objects.bulk_create(temp_data, batch_size=999)
         print('finish bulk_create')
